af69191a53d72b45790cf3524952bd75a50dcb96
adobroselskij <adobroselskij@strongteam.tech>
2025-05-20 10:08:08 +0300

    123

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class Main {
+    public static void main(String[] args) {
+    open("https://dzen.ru/");
+
+    }
+}
